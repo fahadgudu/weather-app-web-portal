407602d86e4c1d2690519af42eac092e96daa977
Usman Ali <usman@applabs.com.au>
2015-05-28 10:02:30 +0500

    RIC-235 : Documentation update

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -4,8 +4,8 @@ class Api::V1::SearchController < ApplicationController
 
   swagger_api :index do
     summary "Returns the search results"
-    notes "Return search results against a section. Where section is either of the 'how_to_grow', 'product', or 'retailer'"
-    param :query, :section,  :string, :required, "either of 'how_to_grow', 'product', or 'retailer'"
+    notes "Return search results against a section. Where section is either of the 'how_to_grow', 'product', 'problem_solver' , or 'retailer'"
+    param :query, :section,  :string, :required, "either of 'how_to_grow', 'product', 'problem_solver', or 'retailer'"
     param :query, :search,   :string, :required, "Term to search against"
     response :unauthorized
   end
