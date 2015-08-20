feb6dd9dd2b8364976c12037f75195ab056b2c19
Zahid Ali <zahid@dapperapps.com.au>
2015-08-20 15:08:42 +0500

    [RIC-394] Alerts are sending/being received twice.
    Find address via postal code from Australia only.

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,6 +1,16 @@
 class Device < ActiveRecord::Base
 
-  geocoded_by :postcode
+  geocoded_by :postcode do |obj,results|
+    if results
+      results.keep_if{|r| r.formatted_address.include?'Australia'}
+      first_match = results.first
+      if results && first_match
+        obj.latitude = first_match.geometry['location']['lat'] rescue nil
+        obj.longitude = first_match.geometry['location']['lng'] rescue nil
+      end
+    end
+  end
+
   reverse_geocoded_by :latitude, :longitude
   validates_uniqueness_of :token
 
