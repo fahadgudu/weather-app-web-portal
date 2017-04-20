982df930b5b0938c5daafe29e29c080380362dd7
fahad-applab <fahad@dapperapps.com.au>
2017-04-20 17:59:22 +0500

    API exposed

--- app/controllers/api/v1/weathers_controller.rb
+++ app/controllers/api/v1/weathers_controller.rb
@@ -6,6 +6,10 @@ class Api::V1::WeathersController < ApplicationController
 
     def show
       @user = User.find params[:id]
+      lat, long = @user.latitude, @user.longitude
+      @weather_data = @user.get_weather_data(lat, long)
+      render :json => @weather_data
     end
+
 
 end
